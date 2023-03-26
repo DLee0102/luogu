7be63cb7c7ae9b68ff7cba06c810ccb172c7868e
DLee0102 <1225390750@qq.com>
2023-03-27 00:30:28 +0800

    2023_3_26

---
+++ datastructure1_2/p1177.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int n,a[1000001];
+//快速排序
+void quick_sort(int s[], int l, int r)
+{
+    if (l < r)
+    {
+        //Swap(s[l], s[(l + r) / 2]); //将中间的这个数和第一个数交换 参见注1
+        int i = l, j = r, x = s[l];
+        while (i < j)
+        {
+            while(i < j && s[j] >= x) // 从右向左找第一个小于x的数
+                j--;  
+            if(i < j) 
+                s[i++] = s[j];
+            
+            while(i < j && s[i] < x) // 从左向右找第一个大于等于x的数
+                i++;  
+            if(i < j) 
+                s[j--] = s[i];
+        }
+        s[i] = x;
+        quick_sort(s, l, i - 1); // 递归调用 
+        quick_sort(s, i + 1, r);
+    }
+}
+int main()
+{
+    cin>>n;
+    for(int i=1;i<=n;i++) cin>>a[i];
+    quick_sort(a, 1, n);
+    for(int i=1;i<=n;i++) cout<<a[i]<<" ";
+}
